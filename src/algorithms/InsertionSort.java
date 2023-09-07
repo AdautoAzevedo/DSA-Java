45e7f1630635bad86e9e7b90cac036e0f7d2019f
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2023-09-07 15:18:04 -0300

    Added Insertion Sort

---
+++ src/algorithms/InsertionSort.java
@@ -0,0 +1,34 @@
+package algorithms;
+
+public class InsertionSort {
+    public static int[] insertionSort(int[] array){
+
+        //This algorithm works by taking one element from the unsorted portion of the array and inserts it into its correct position
+
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j>=0 && array[j] > key) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+           for (int j2 = 0; j2 < array.length; j2++) {
+           }
+        }
+
+        return array;
+    }
+    public static void main(String[] args) {
+        int[] array = {30,50,20,60,40,10};
+
+        int[] sortedArray;
+
+        sortedArray =insertionSort(array);
+
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.print(array[i] +" ");
+        }
+    }
+}
